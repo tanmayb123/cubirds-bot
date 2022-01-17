18dc10751fbf9564480ed6c4750ebc011fb718a0
Tanmay Bakshi <tajymany@gmail.com>
2022-01-17 02:52:39 -0500

    multithreading

--- src/main.rs
+++ src/main.rs
@@ -11,16 +11,21 @@ use std::str::FromStr;
 use std::io::stdin;
 use std::time::Instant;
 use std::hash::Hash;
+use std::sync::mpsc;
+use std::thread;
 use crate::bird::Bird;
 use crate::line::Line;
 use crate::state::CubirdsState;
 use crate::materialized_state::MaterializedCubirdsState;
 use crate::materialized_state::SimplifiableMove;
+use crate::materialized_state::FlockMove;
+use crate::materialized_state::LineMove;
 
 const LINES: usize = 4;
 const STARTING_CARDS_HAND: i32 = 8;
+const THREADS: i32 = 8;
 
-fn evaluate_state<T: SimplifiableMove + Eq + Hash>(state: &CubirdsState, rollout_func: fn(&mut MaterializedCubirdsState) -> Option<(T, bool)>) {
+fn internal_evaluate_state<T: SimplifiableMove + Eq + Hash>(state: &CubirdsState, rollout_func: fn(&mut MaterializedCubirdsState) -> Option<(T, bool)>) -> HashMap<T, (i32, i32)> {
     let mut move_scores = HashMap::new();
 
     let now = Instant::now();
@@ -32,49 +37,64 @@ fn evaluate_state<T: SimplifiableMove + Eq + Hash>(state: &CubirdsState, rollout
                 score.0 += 1;
             }
             score.1 += 1;
-
-            let mut scores = Vec::new();
-            for (fmove, winrate) in &move_scores {
-                let score = (winrate.0 as f64) / (winrate.1 as f64);
-                scores.push((fmove.simplified(), score * 100.0));
-            }
-            scores.sort_by(|x, y| y.1.partial_cmp(&x.1).unwrap());
-            for i in 0..5 {
-                if i == scores.len() {
-                    break;
-                }
-                print!("{} ({}%) ", scores[i].0, scores[i].1);
-            }
-            print!("\r");
         }
     }
 
-    let mut scores = Vec::new();
-    for (fmove, winrate) in &move_scores {
-        let score = (winrate.0 as f64) / (winrate.1 as f64);
-        scores.push((fmove.simplified(), score * 100.0));
-    }
-    scores.sort_by(|x, y| y.1.partial_cmp(&x.1).unwrap());
-    for i in 0..5 {
-        if i == scores.len() {
-            break;
+    return move_scores;
+}
+
+fn evaluate_state_thread_lm(rx: mpsc::Receiver<CubirdsState>, tx: mpsc::Sender<HashMap<LineMove, (i32, i32)>>) {
+    thread::spawn(move || {
+        for state in rx {
+            let evaluation = internal_evaluate_state(&state, |x| x.full_rollout());
+            tx.send(evaluation).unwrap();
         }
-        print!("{} ({}%) ", scores[i].0, scores[i].1);
-    }
-    print!("\n");
+    });
+}
+
+fn evaluate_state_thread_fm(rx: mpsc::Receiver<CubirdsState>, tx: mpsc::Sender<HashMap<FlockMove, (i32, i32)>>) {
+    thread::spawn(move || {
+        for state in rx {
+            let evaluation = internal_evaluate_state(&state, |x| x.flock_rollout());
+            tx.send(evaluation).unwrap();
+        }
+    });
 }
 
 pub struct CommandHandler {
     pub state: CubirdsState,
     prev_state: Option<CubirdsState>,
+    full_tx: mpsc::Receiver<HashMap<LineMove, (i32, i32)>>,
+    flock_tx: mpsc::Receiver<HashMap<FlockMove, (i32, i32)>>,
+    full_rxs: Vec<mpsc::Sender<CubirdsState>>,
+    flock_rxs: Vec<mpsc::Sender<CubirdsState>>,
 }
 
 impl CommandHandler {
     fn from(state: CubirdsState) -> CommandHandler {
-        CommandHandler {
+        let mut full_threads = Vec::new();
+        let (lm_tx, lm_rx) = mpsc::channel();
+        for _ in 0..THREADS {
+            let (tx, rx) = mpsc::channel();
+            full_threads.push(tx);
+            evaluate_state_thread_lm(rx, lm_tx.clone());
+        }
+        let mut flock_threads = Vec::new();
+        let (fm_tx, fm_rx) = mpsc::channel();
+        for _ in 0..THREADS {
+            let (tx, rx) = mpsc::channel();
+            flock_threads.push(tx);
+            evaluate_state_thread_fm(rx, fm_tx.clone());
+        }
+        let handler = CommandHandler {
             state: state,
             prev_state: None,
-        }
+            full_tx: lm_rx,
+            flock_tx: fm_rx,
+            full_rxs: full_threads,
+            flock_rxs: flock_threads,
+        };
+        return handler;
     }
 
     fn handle_play(&mut self, components: Vec<&str>) {
@@ -122,11 +142,54 @@ impl CommandHandler {
     }
 
     fn handle_play_score(&mut self) {
-        evaluate_state(&self.state, |x| x.full_rollout());
+        for tx in &self.full_rxs {
+            tx.send(self.state.clone()).unwrap();
+        }
+        let mut all_scores: HashMap<String, (i32, i32)> = HashMap::new();
+        for _ in 0..THREADS {
+            let scores = self.full_tx.recv().unwrap();
+            for (fmove, score) in scores {
+                let entry = all_scores.entry(fmove.simplified()).or_insert((0, 0));
+                entry.0 += score.0;
+                entry.1 += score.1;
+            }
+        }
+        CommandHandler::print_scores(all_scores);
     }
 
     fn handle_flock_score(&mut self) {
-        evaluate_state(&self.state, |x| x.flock_rollout());
+        for tx in &self.flock_rxs {
+            tx.send(self.state.clone()).unwrap();
+        }
+        let mut all_scores: HashMap<String, (i32, i32)> = HashMap::new();
+        for _ in 0..THREADS {
+            let scores = self.flock_tx.recv().unwrap();
+            for (fmove, score) in scores {
+                let entry = all_scores.entry(fmove.simplified()).or_insert((0, 0));
+                entry.0 += score.0;
+                entry.1 += score.1;
+            }
+        }
+        CommandHandler::print_scores(all_scores);
+    }
+
+    fn print_scores(move_scores: HashMap<String, (i32, i32)>) {
+        let mut scores = Vec::new();
+        let mut total = 0;
+        for (fmove, winrate) in &move_scores {
+            let score = (winrate.0 as f64) / (winrate.1 as f64);
+            total += winrate.1;
+            scores.push((fmove, score * 100.0));
+        }
+        println!("evaluated {}", total);
+        scores.sort_by(|x, y| y.1.partial_cmp(&x.1).unwrap());
+        for i in 0..5 {
+            if i == scores.len() {
+                break;
+            }
+            print!("{} ({}%) ", scores[i].0, scores[i].1);
+        }
+        print!("\n");
     }
 
     fn handle_undo(&mut self) {
